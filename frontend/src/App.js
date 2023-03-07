724de84d6e3a08c6f1099562d005b205f1ff0869
Ayush rana <ayushrana200177@gmail.com>
2023-03-07 10:05:29 +0530

    add bootstarp to the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,35 @@ import ContactUs from "./components/ContactUs";
 import HomeScreen from "./components/HomeScreen";
 import ProductScreen from "./components/ProductScreen";
 import ChatBotScreen from "./components/ChatBot/ChatBotScreen";
+import NavBar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">Soul-Merch</Link>
-          <Link to="/contact">Contact Us</Link>
+          <NavBar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <NavBar.Brand>Soul-Merch</NavBar.Brand>
+              </LinkContainer>
+            </Container>
+          </NavBar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/chatbot" element={<ChatBotScreen />} />
-            <Route path="/contact" element={<ContactUs />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/chatbot" element={<ChatBotScreen />} />
+              <Route path="/contact" element={<ContactUs />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All right reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
