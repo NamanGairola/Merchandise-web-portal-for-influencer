073ac44bf01e12deb21738f2383c775019082293
Ayush rana <ayushrana200177@gmail.com>
2023-03-12 23:08:25 +0530

    created user auchiaction system

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import data from "./data.js";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config();
 
@@ -15,26 +18,16 @@ mongoose
   });
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
+app.use("/api/users", userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 4000;
