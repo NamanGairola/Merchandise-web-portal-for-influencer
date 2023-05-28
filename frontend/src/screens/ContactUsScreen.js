f0fd549c3359920340f801189c1fbf646703a91b
Ayush rana <ayushrana200177@gmail.com>
2023-05-28 17:43:00 +0530

    update contact page

--- frontend/src/screens/ContactUsScreen.js
+++ frontend/src/screens/ContactUsScreen.js
@@ -1,13 +1,64 @@
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
+import { FaMapMarkerAlt, FaPhone, FaEnvelope } from "react-icons/fa";
 
 function ContactUsScreen() {
   return (
-    <div>
-      <h1>ContactUs</h1>
-      <LinkContainer to="/chatbot">
-        <h4>talk with the bot</h4>
-      </LinkContainer>
+    <div
+      style={{
+        minHeight: "80vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <div>
+        <h1>ContactUs</h1>
+        <LinkContainer to="/chatbot">
+          <Link
+            className="nav-link"
+            style={{
+              color: "black",
+              textDecoration: "none",
+              display: "inline-block",
+              padding: "8px 16px",
+              backgroundColor: "#ebebeb",
+              borderRadius: "4px",
+              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+              transition: "background-color 0.3s ease",
+            }}
+          >
+            <h3 style={{ color: "black", textDecoration: "none" }}>
+              Talk with the bot
+            </h3>
+          </Link>
+        </LinkContainer>
+      </div>
+
+      <div>
+        <Row>
+          <Col className="text-center">
+            <ul className="list-unstyled d-flex justify-content-between">
+              <li>
+                <FaMapMarkerAlt className="fa-2x" />
+                <p>Dehradun, 248001, Uttarakhand, India</p>
+              </li>
+              <li>
+                <FaPhone className="mt-4 fa-2x" />
+                <p>+ 2387876787</p>
+              </li>
+              <li>
+                <FaEnvelope className="mt-4 fa-2x" />
+                <p>contact@majarproject.com</p>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
+
 export default ContactUsScreen;
